12afddcb61369744667b5c3d4c22d8dbde56f242
iizhukov <iizhukov@icloud.com>
2025-09-04 08:34:52 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/home';
+import './styles/global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
+
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded fs-4">
+      Tailwind работает!
+    </div>
+  );
+};
+
+export default Home;
